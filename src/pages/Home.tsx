5d3261d9ab3de12d4cd769233929372d1301558e
Mateus Leonardo <m4teusmatos@gmail.com>
2022-05-12 23:07:32 -0300

    initial commi

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Home = () => {
+  return (
+    <View>
+      <Text style={{color: '#333'}}>Home</Text>
+    </View>
+  );
+};
+
+export default Home;
